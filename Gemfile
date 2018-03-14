555123a0322449392d1092b27938bd62c56ee8f0
Unknown <steven.bengtson@me.com>
2018-03-14 13:58:08 -0700

    Add swagger docs

--- Gemfile
+++ Gemfile
@@ -7,8 +7,10 @@ end
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.5'
+
 # Use postgresql as the database for Active Record
 gem 'pg', '>= 0.18', '< 2.0'
+
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
 
@@ -31,6 +33,9 @@ gem 'rack-attack', '~> 5.1.0'
 # https://github.com/jwt/ruby-jwt
 gem 'jwt', '~> 2.1.0'
 
+# https://github.com/richhollis/swagger-docs
+gem 'swagger-docs', '~> 0.2.9'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
@@ -42,11 +47,11 @@ group :development do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
 
-    # Use RSpec for specs
-    gem 'rspec-rails', '~> 3.7.2'
+  # Use RSpec for specs
+  gem 'rspec-rails', '~> 3.7.2'
 
-    # Use Factory Girl for generating random test data
-    gem 'factory_bot_rails', '~> 4.8.2'
+  # Use Factory Girl for generating random test data
+  gem 'factory_bot_rails', '~> 4.8.2'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,37 @@
 module Api::V1
   class UsersController < ApplicationController
+    swagger_controller :users, "User Management"
+
     before_action :authenticate_request!
     before_action :set_user, only: [:show, :update, :destroy]
 
+    def self.add_common_params(api)
+      api.param :form, "user[name]", :string, :required, "Name"
+      api.param :form, "user[email]", :string, :required, "Email"
+    end
+
+    swagger_api :index do
+      summary "Fetches all User items"
+      notes "This lists all the active users"
+    end
+
+    swagger_api :show do
+      summary "Fetches user by id"
+      notes "Find user by id"
+      param :path, :id, :integer, :optional, "User Id"
+      response :unauthorized
+      response :not_acceptable, "The request you made is not acceptable"
+      response :requested_range_not_satisfiable
+    end
+
+    swagger_api :create do |api|
+      summary "Create a new User item"
+      notes "Create a new User item"
+      Api::V1::UsersController::add_common_params(api)
+      response :unauthorized
+      response :not_acceptable
+      response :unprocessable_entity
+    end
     # GET /users
     def index
       @users = User.all
